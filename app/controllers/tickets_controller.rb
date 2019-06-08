f50dc338a2e6ddc62a057266eb212fd649924cea
Johnny Chan <johchan24@gmail.com>
2019-06-08 17:34:18 +1000

    created index page for tickets, installed will_paginate gem(have to require, pending gem installation, not working for some reason)

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,14 @@
 class TicketsController < ApplicationController
+require 'will_paginate/array'
+
   def index
-    @tickets = User::TICKETS.tickets
+    @tickets = get_tickets.tickets.to_a
+    @tickets = @tickets.paginate(page: params[:page], per_page: 25)
+  end
+
+  private
+
+  def get_tickets
+    User::TICKETS
   end
 end
