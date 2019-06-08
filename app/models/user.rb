389f2b32f36f213c51959fbe9c03cb57d9b26fac
Johnny Chan <johchan24@gmail.com>
2019-06-08 14:22:46 +1000

    created routes, root is index page, added index to ticket controller

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  TICKETS = ZendeskAPI::Client.new do |config|
+    config.url = "https://jchan.zendesk.com/api/v2"
+
+    config.username = "johchan24@gmail.com"
+
+    config.token = "7pRnKgIuglQBcWrWjAww4Yop0cAT8ltnrCqybXvo"
+  end
+end
