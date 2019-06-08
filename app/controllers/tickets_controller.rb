69a9d0b77618e9bb1f74b1d6749dd1796cb83b98
Johnny Chan <johchan24@gmail.com>
2019-06-08 17:56:35 +1000

    created show method in controller and link from index to individual tickets with params id

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,13 +2,17 @@ class TicketsController < ApplicationController
 require 'will_paginate/array'
 
   def index
-    @tickets = get_tickets.tickets.to_a
+    @tickets = get_tickets.to_a
     @tickets = @tickets.paginate(page: params[:page], per_page: 25)
+  end
+
+  def show
+    @ticket = get_tickets.find(id: params[:id])
   end
 
   private
 
   def get_tickets
-    User::TICKETS
+    User::TICKETS.tickets
   end
 end
