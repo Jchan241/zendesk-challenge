5dea118a70650eb0a4cea366cbeac3774539c494
Johnny Chan <johchan24@gmail.com>
2019-06-09 22:02:56 +1000

    added test scenarios to test file

--- test/system/tickets_test.rb
+++ test/system/tickets_test.rb
@@ -1,16 +1,20 @@
 require "application_system_test_case"
 
 class TicketsTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "visiting the root, should render index" do
     visit "/"
     assert_selector "h1", text: "Zendesk Ticket Viewer"
     assert_selector ".ticket-link", count: 25
   end
 
+  test "index should render correct template and layout" do
+    visit :index
+    assert_template :index
+  end
+
   test "visiting a ticket that doesn't exist" do
-    visit "/"
-    assert_selector "h1", text: "Zendesk Ticket Viewer"
-    assert_selector ".ticket-link", count: 25
+    visit "/tickets/123456789"
+    assert_selector "h3", text: "Record Not Found Error"
   end
 
 
